0ad8a754fb40d317be757ca8b96ab949a7c00f12
Josh Baker <joshb99559@gmail.com>
2018-08-02 22:31:29 -0400

    Added State and Conditionals

--- src/com/thelambdacomplex/renn/Expr.java
+++ src/com/thelambdacomplex/renn/Expr.java
@@ -4,65 +4,103 @@ import java.util.List;
 
 abstract class Expr {
     interface Visitor<R> {
-        R visitBinaryExpr(Binary expr);
-        R visitGroupingExpr(Grouping expr);
-        R visitLiteralExpr(Literal expr);
-        R visitUnaryExpr(Unary expr);
+    R visitAssignExpr(Assign expr);
+    R visitBinaryExpr(Binary expr);
+    R visitGroupingExpr(Grouping expr);
+    R visitLiteralExpr(Literal expr);
+    R visitLogicalExpr(Logical expr);
+    R visitUnaryExpr(Unary expr);
+    R visitVariableExpr(Variable expr);
     }
-    
-    static class Binary extends Expr {
-        Binary(Expr left, Token operator, Expr right) {
-            this.left = left;
-            this.operator = operator;
-            this.right = right;
-        }
+ static class Assign extends Expr {
+    Assign(Token name, Expr value) {
+    this.name = name;
+    this.value = value;
+    }
 
-        <R> R accept(Visitor<R> visitor) {
-            return visitor.visitBinaryExpr(this);
-        }
+    <R> R accept(Visitor<R> visitor) {
+    return visitor.visitAssignExpr(this);
+    }
 
-        final Expr left;
-        final Token operator;
-        final Expr right;
+    final Token name;
+    final Expr value;
+  }
+ static class Binary extends Expr {
+    Binary(Expr left, Token operator, Expr right) {
+    this.left = left;
+    this.operator = operator;
+    this.right = right;
     }
- 
-    static class Grouping extends Expr {
-        Grouping(Expr expression) {
-            this.expression = expression;
-        }
-    
-        <R> R accept(Visitor<R> visitor) {
-            return visitor.visitGroupingExpr(this);
-        }
-    
-        final Expr expression;
+
+    <R> R accept(Visitor<R> visitor) {
+    return visitor.visitBinaryExpr(this);
     }
-    
-    static class Literal extends Expr {
-        Literal(Object value) {
-            this.value = value;
-        }
-    
-        <R> R accept(Visitor<R> visitor) {
-            return visitor.visitLiteralExpr(this);
-        }
-    
-        final Object value;
+
+    final Expr left;
+    final Token operator;
+    final Expr right;
+  }
+ static class Grouping extends Expr {
+    Grouping(Expr expression) {
+    this.expression = expression;
     }
-    
-    static class Unary extends Expr {
-        Unary(Token operator, Expr right) {
-            this.operator = operator;
-            this.right = right;
-        }
-    
-        <R> R accept(Visitor<R> visitor) {
-            return visitor.visitUnaryExpr(this);
-        }
-    
-        final Token operator;
-        final Expr right;
+
+    <R> R accept(Visitor<R> visitor) {
+    return visitor.visitGroupingExpr(this);
     }
-    
-    abstract <R> R accept(Visitor<R> visitor);
+
+    final Expr expression;
+  }
+ static class Literal extends Expr {
+    Literal(Object value) {
+    this.value = value;
+    }
+
+    <R> R accept(Visitor<R> visitor) {
+    return visitor.visitLiteralExpr(this);
+    }
+
+    final Object value;
+  }
+ static class Logical extends Expr {
+    Logical(Expr left, Token operator, Expr right) {
+    this.left = left;
+    this.operator = operator;
+    this.right = right;
+    }
+
+    <R> R accept(Visitor<R> visitor) {
+    return visitor.visitLogicalExpr(this);
+    }
+
+    final Expr left;
+    final Token operator;
+    final Expr right;
+  }
+ static class Unary extends Expr {
+    Unary(Token operator, Expr right) {
+    this.operator = operator;
+    this.right = right;
+    }
+
+    <R> R accept(Visitor<R> visitor) {
+    return visitor.visitUnaryExpr(this);
+    }
+
+    final Token operator;
+    final Expr right;
+  }
+ static class Variable extends Expr {
+    Variable(Token name) {
+    this.name = name;
+    }
+
+    <R> R accept(Visitor<R> visitor) {
+    return visitor.visitVariableExpr(this);
+    }
+
+    final Token name;
+  }
+
+ abstract <R> R accept(Visitor<R> visitor);
 }
--- src/com/thelambdacomplex/renn/Interpreter.java
+++ src/com/thelambdacomplex/renn/Interpreter.java
@@ -1,11 +1,14 @@
 package com.thelambdacomplex.renn;
+import java.util.List;
 
-public class Interpreter implements Expr.Visitor<Object> {
-
-    void interpret(Expr expression) {
+public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+    private Environment environment = new Environment();
+    
+    void interpret(List<Stmt> statements) {
         try {
-            Object value = evaluate(expression);
-            System.out.println(stringify(value));
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
         } catch (RuntimeError error) {
             Renn.runtimeError(error);
         }
@@ -14,6 +17,19 @@ public class Interpreter implements Expr.Visitor<Object> {
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
         return expr.value;
+    }
+    
+    @Override
+    public Object visitLogicalExpr(Expr.Logical expr) {
+        Object left = evaluate(expr.left);
+        
+        if (expr.operator.type == TokenType.OR) {
+            if (isTruthy(left)) return left;
+        } else {
+            if (!isTruthy(left)) return left;
+        }
+        
+        return evaluate(expr.right);
     }
     
     @Override
@@ -59,6 +75,9 @@ public class Interpreter implements Expr.Visitor<Object> {
             return (double)left - (double)right;
           case SLASH:
             checkNumberOperands(expr.operator, left, right);
+            if ((double)right == 0) {
+                throw new RuntimeError(expr.operator, "Can not divide by zero.");
+            }
             return (double)left / (double)right;
           case STAR:
             checkNumberOperands(expr.operator, left, right);
@@ -71,7 +90,15 @@ public class Interpreter implements Expr.Visitor<Object> {
             if (left instanceof String && right instanceof String) {
                 return (String)left + (String)right;
             }
-            throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
+            
+            if (left instanceof String && right instanceof Double) {
+                return (String)left + stringify(right);
+            }
+            
+            if (left instanceof Double && right instanceof String) {
+                return stringify(left) + (String)right;
+            }
+            throw new RuntimeError(expr.operator, "Operands must be a combination of numbers or strings.");
           case BANG_EQUAL: return !isEqual(left, right);
           case EQUAL_EQUAL: return isEqual(left, right);
         }
@@ -91,6 +118,84 @@ public class Interpreter implements Expr.Visitor<Object> {
     
     private Object evaluate(Expr expr) {
         return expr.accept(this);
+    }
+    
+    private void execute(Stmt stmt) {
+        stmt.accept(this);
+    }
+    
+    void executeBlock(List<Stmt> statements, Environment environment) {
+        Environment previous = this.environment;
+        try {
+            this.environment = environment;
+            
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
+        } finally {
+            this.environment = previous;
+        }
+    }
+    
+    @Override
+    public Void visitBlockStmt(Stmt.Block stmt) {
+        executeBlock(stmt.statements, new Environment(environment));
+        return null;
+    }
+    
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        evaluate(stmt.expression);
+        return null;
+    }
+    
+    @Override
+    public Void visitIfStmt(Stmt.If stmt) {
+        if (isTruthy(evaluate(stmt.condition))) {
+            execute(stmt.thenBranch);
+        } else if (stmt.elseBranch != null) {
+            execute(stmt.elseBranch);
+        }
+        return null;
+    }
+    
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt) {
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
+        return null;
+    }
+    
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt) {
+        Object value = null;
+        if (stmt.initializer != null) {
+            value = evaluate(stmt.initializer);
+        }
+        
+        environment.define(stmt.name.lexeme, value);
+        return null;
+    }
+    
+    @Override
+    public Void visitWhileStmt(Stmt.While stmt) {
+        while (isTruthy(evaluate(stmt.condition))) {
+            execute(stmt.body);
+        }
+        return null;
+    }
+    
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        Object value = evaluate(expr.value);
+        
+        environment.assign(expr.name, value);
+        return value;
+    }
+    
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr) {
+        return environment.get(expr.name);
     }
     
     private boolean isTruthy(Object object) {
